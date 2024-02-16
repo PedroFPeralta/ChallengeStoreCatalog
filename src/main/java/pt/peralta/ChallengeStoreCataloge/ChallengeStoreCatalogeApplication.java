c7e98bbf04380c2a89feff2e3a5872e0cc492bb6
PedroFPeralta <200221137@estudantes.ips.pt>
2024-02-16 18:53:09 +0000

    Init Project

---
+++ src/main/java/pt/peralta/ChallengeStoreCataloge/ChallengeStoreCatalogeApplication.java
@@ -0,0 +1,13 @@
+package pt.peralta.ChallengeStoreCataloge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeStoreCatalogeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeStoreCatalogeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/peralta/ChallengeStoreCataloge/ChallengeStoreCatalogeApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.peralta.ChallengeStoreCataloge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeStoreCatalogeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
